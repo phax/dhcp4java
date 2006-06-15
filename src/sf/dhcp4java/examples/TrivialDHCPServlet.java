090ee274daaeb208bf9a22aa2b8f624b68ef4010
shadinger <shadinger@users.noreply.github.com>
2006-06-15 09:29:33 +0000

    Code cleanup

--- src/sf/dhcp4java/examples/TrivialDHCPServlet.java
+++ src/sf/dhcp4java/examples/TrivialDHCPServlet.java
@@ -21,14 +21,17 @@ package sf.dhcp4java.examples;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import sf.dhcp4java.DHCPConstants;
+import sf.dhcp4java.DHCPOption;
 import sf.dhcp4java.DHCPPacket;
 import sf.dhcp4java.DHCPServer;
 import sf.dhcp4java.DHCPServlet;
 import sf.dhcp4java.DHCPServerInitException;
+
+import static sf.dhcp4java.DHCPConstants.*;
 
 
 /**
@@ -41,95 +44,98 @@ public class TrivialDHCPServlet extends DHCPServlet {
 
     private static final Logger logger = Logger.getLogger("sf.dhcp4java.examples.trivialdhcpservlet");
     
+    DHCPOption[] commonOptions = null;
+    
     /* (non-Javadoc)
-     * @see sf.dhcp4java.DHCPServer#service(sf.dhcp4java.DHCPPacket)
-     */
-    public DHCPPacket service(DHCPPacket request) {
-        
-        logger.info("*** Received from "+DHCPPacket.getHostAddress(request.getAddress())+
-                			" ("+request.getPort()+")");
-        if (logger.isLoggable(Level.FINER))		logger.finer(request.toString());
-        
-        if (!request.isDhcp()) return null;			// only BOOTP packet
-        Byte messageTypeByte = request.getOptionAsByte(DHCPConstants.DHO_DHCP_MESSAGE_TYPE);
-        if (messageTypeByte == null) return null;	// no DHCP message type
-        
-        byte messageType = messageTypeByte.byteValue();
-        if ((messageType != DHCPConstants.DHCPDISCOVER) &&
-            (messageType != DHCPConstants.DHCPREQUEST))
-            	return null;		// only DISCOVER and REQUEST are treated
-        
-        String vendor = request.getOptionAsString(DHCPConstants.DHO_VENDOR_CLASS_IDENTIFIER);
-        if ((vendor == null) || (vendor.indexOf("sagem") < 0))
-            return null;		// only "sagem" vendor class
-        
-        DHCPPacket response = new DHCPPacket(); 
-        
-        // copy addresses
-        response.setAddress(request.getAddress());
-        response.setPort(request.getPort());
-        
-        response.setComment(request.getComment());
-        
-        response.setOp(DHCPConstants.BOOTREPLY);
-        response.setHtype(request.getHtype());
-        response.setHlen(request.getHlen());
-        response.setHops(request.getHops());
-        response.setXid(request.getXid());
-        response.setSecs(request.getSecs());
-        response.setFlags(request.getFlags());
-        response.setCiaddrRaw(request.getCiaddrRaw());
-        //response.setYiaddrRaw(request.getYiaddrRaw());
-        response.setSiaddrRaw(request.getSiaddrRaw());
-        response.setGiaddrRaw(request.getGiaddrRaw());
-        response.setChaddr(request.getChaddr());
-        
-        byte[] yiaddr = { (byte) 10, (byte) 1, (byte) 1, (byte) 23 };
-        response.setYiaddrRaw(yiaddr);
-        
-        // messagetype ?
-        switch (messageType) {
-        	case DHCPConstants.DHCPDISCOVER:
-        	    response.setOptionAsByte(DHCPConstants.DHO_DHCP_MESSAGE_TYPE, DHCPConstants.DHCPOFFER);
-        		break;
-        	case DHCPConstants.DHCPREQUEST:
-        	    response.setOptionAsByte(DHCPConstants.DHO_DHCP_MESSAGE_TYPE, DHCPConstants.DHCPACK);
-        		break;
-        	default:
-        	    return null;		// unsupported message type
-        }
-        
-        // set some options
-        try {
-            response.setOptionAsInetAddress(DHCPConstants.DHO_DHCP_SERVER_IDENTIFIER,
-                    InetAddress.getByName("192.168.1.1"));
-            response.setOptionAsInt(DHCPConstants.DHO_DHCP_LEASE_TIME, 86400);
-            response.setOptionAsInetAddress(DHCPConstants.DHO_SUBNET_MASK,
-                    InetAddress.getByName("255.255.252.0"));
-            response.setOptionAsInetAddress(DHCPConstants.DHO_ROUTERS,
-                    InetAddress.getByName("10.0.0.10"));
-            response.setOptionAsInetAddress(DHCPConstants.DHO_STATIC_ROUTES,
-                    InetAddress.getByName("172.20.224.167")); // TODO multiple INETADDRS
-            response.setOptionAsInetAddress(DHCPConstants.DHO_NTP_SERVERS,
-                    InetAddress.getByName("10.0.0.10"));
-            response.setOptionAsInetAddress(DHCPConstants.DHO_WWW_SERVER,
-                    InetAddress.getByName("10.0.0.10"));
-        } catch (UnknownHostException e) {
-            // too bad
-        }
-        
-        // copy option 82
-        response.setOptionRaw(DHCPConstants.DHO_DHCP_AGENT_OPTIONS,
-                	request.getOptionRaw(DHCPConstants.DHO_DHCP_AGENT_OPTIONS));
-        
-        logger.info("+++Sending back to "+DHCPPacket.getHostAddress(response.getAddress())
-                			+" ("+response.getPort()+")");
-        if (logger.isLoggable(Level.FINER))		logger.finer(response.toString());
-        
-        return response;
-    }
+	 * @see sf.dhcp4java.DHCPServlet#init(java.util.Properties)
+	 */
+	@Override
+	public void init(Properties props) {
+		// we create a dummy packet to extract "common options"
+		DHCPPacket temp = new DHCPPacket();
+		try {
+			temp.setOptionAsInetAddress(DHO_DHCP_SERVER_IDENTIFIER, "192.168.1.1");
+			temp.setOptionAsInt(DHO_DHCP_LEASE_TIME, 86400);
+			temp.setOptionAsInetAddress(DHO_SUBNET_MASK, "255.255.252.0");
+			temp.setOptionAsInetAddress(DHO_ROUTERS, "10.0.0.10");
+			temp.setOptionAsInetAddress(DHO_STATIC_ROUTES, "172.20.224.167");
+			temp.setOptionAsInetAddress(DHO_NTP_SERVERS, "10.0.0.10");
+			temp.setOptionAsInetAddress(DHO_WWW_SERVER, "10.0.0.10");
+			// store options in a instance array
+			commonOptions = temp.getOptionsArray();
+		} catch (UnknownHostException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    public static void main(String[] args) {
+	/* (non-Javadoc)
+	 * @see sf.dhcp4java.DHCPServlet#doDiscover(sf.dhcp4java.DHCPPacket)
+	 */
+	@Override
+	protected DHCPPacket doDiscover(DHCPPacket request) {
+		InetAddress clientIp = calcAddrFromMac(request);
+		if (clientIp == null)	return null;
+		
+		DHCPPacket response = request.clone();
+		response.setOp(BOOTREPLY);
+		response.setDHCPMessageType(DHCPOFFER);
+		response.setOptions(commonOptions);
+
+		return response;
+	}
+
+	/* (non-Javadoc)
+	 * @see sf.dhcp4java.DHCPServlet#doRequest(sf.dhcp4java.DHCPPacket)
+	 */
+	@Override
+	protected DHCPPacket doRequest(DHCPPacket request) {
+		InetAddress clientIp = calcAddrFromMac(request);
+		if (clientIp == null)	return null;
+		
+		DHCPPacket response = request.clone();
+		response.setOp(BOOTREPLY);
+		response.setDHCPMessageType(DHCPOFFER);
+		response.setOptions(commonOptions);
+
+		return response;
+	}
+
+	/**
+	 * Calculate address from packet and @MAC Address.
+	 * 
+	 * @param request
+	 * @return address for client, or null if ignore
+	 */
+	private InetAddress calcAddrFromMac(DHCPPacket request) {
+		try {
+			// check vendor class
+			String vendor = request.getOptionAsString(DHO_VENDOR_CLASS_IDENTIFIER);
+			if ((vendor == null) || (vendor.indexOf("MSFT5.0") < 0))
+				return null;		// only Microsoft vendor class
+	
+			// check @MAC address format
+			if ((request.getHtype() != HTYPE_ETHER) && (request.getHlen() != 6))
+				return null;
+			
+			// check if we know the @MAC address
+			String macAddrHex = request.getChaddrAsHex();
+			
+			// dummy
+			if (macAddrHex.equals("000802E7BFA5")) {
+				return InetAddress.getByName("10.0.0.1");
+			}
+		} catch (UnknownHostException e) {
+			logger.log(Level.SEVERE, "Unexpected exception", e);
+		}
+		
+		return null;
+	}
+	/**
+	 * Server launcher.
+	 * 
+	 * @param args command line arguments - ignored
+	 */
+	public static void main(String[] args) {
         try {
             DHCPServer server = DHCPServer.initServer(new TrivialDHCPServlet(), null);
             logger.setLevel(Level.OFF);
