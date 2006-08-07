0896aedee0bf4c07d2a3b48d237b96af7e8cdfd9
shadinger <shadinger@users.noreply.github.com>
2006-08-07 08:42:33 +0000

    Switched license from GPL to Lesser GPL.

--- src/sf/dhcp4java/examples/DHCPSnifferServlet.java
+++ src/sf/dhcp4java/examples/DHCPSnifferServlet.java
@@ -1,21 +1,20 @@
 /*
- *	This file is part of dhcp4java.
+ *	This file is part of dhcp4java, a DHCP API for the Java language.
+ *	(c) 2006 Stephan Hadinger
  *
- *	dhcp4java is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation; either version 2 of the License, or
- *	(at your option) any later version.
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation; either
+ *	version 2.1 of the License, or (at your option) any later version.
  *
- *	dhcp4java is distributed in the hope that it will be useful,
+ *	This library is distributed in the hope that it will be useful,
  *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *	Lesser General Public License for more details.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with Foobar; if not, write to the Free Software
- *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * (c) 2006 Stephan Hadinger
+ *	You should have received a copy of the GNU Lesser General Public
+ *	License along with this library; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 package sf.dhcp4java.examples;
 import java.util.logging.Level;
