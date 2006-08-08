fc33e8d700496cd2fed27adb0fb1dd22f36f73fc
shadinger <shadinger@users.noreply.github.com>
2006-08-08 16:26:09 +0000

    RFC 2937.

--- src/sf/dhcp4java/DHCPConstants.java
+++ src/sf/dhcp4java/DHCPConstants.java
@@ -149,7 +149,7 @@ public class DHCPConstants {
     public static final byte DHO_ASSOCIATED_IP				 =  92; // rfc 4388
     public static final byte DHO_USER_AUTHENTICATION_PROTOCOL =  98;
     public static final byte DHO_AUTO_CONFIGURE               = 116;
-    public static final byte DHO_NAME_SERVICE_SEARCH          = 117;
+    public static final byte DHO_NAME_SERVICE_SEARCH          = 117; // rfc 2937
     public static final byte DHO_SUBNET_SELECTION             = 118; // rfc 3011
     public static final byte DHO_DOMAIN_SEARCH	             = 119; // rfc 3397
     public static final byte DHO_END                          =  -1;
