9c21dec73fc7c7b0e1445f959ad2badc8d89b8c9
shadinger <shadinger@users.noreply.github.com>
2006-09-26 12:46:32 +0000

    Work in progress.

--- src/org/dhcp4java/server/config/xml/GlobalConfigReader.java
+++ src/org/dhcp4java/server/config/xml/GlobalConfigReader.java
@@ -23,13 +23,18 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 import org.dhcp4java.DHCPConstants;
+import org.dhcp4java.DHCPOption;
+import org.dhcp4java.DHCPPacket;
 import org.dhcp4java.InetCidr;
 import org.dhcp4java.server.AddressRange;
 import org.dhcp4java.server.Subnet;
@@ -110,73 +115,17 @@ public final class GlobalConfigReader {
 					
 					// look for ranges
 					Elements ranges = subnetElt.getChildElements("range");
-					for (int j=0; j<ranges.size(); j++) {
-						AddressRange range = null;
-						try {
-							Element rangeElt = ranges.get(j);
-							String rangeStart = rangeElt.getAttributeValue("start");
-							String rangeEnd = rangeElt.getAttributeValue("end");
-							if (rangeStart == null) {
-								throw new ConfigException("range @start missing in "+getElementPath(rangeElt));
-							}
-							if (rangeEnd == null) {
-								throw new ConfigException("range @end missing in "+getElementPath(rangeElt));
-							}
-							if (logger.isLoggable(Level.FINEST)) {
-								logger.finest("range start: "+rangeStart+", range end: "+rangeEnd+", from "+getElementPath(rangeElt));
-							}
-							
-							range = new AddressRange(InetAddress.getByName(rangeStart),
-													 InetAddress.getByName(rangeEnd));
-						} catch (ConfigException e) {
-							logger.log(Level.WARNING, "address range is invalide", e);
-						} finally {
-							if (range != null) {
-								subnet.getAddrRanges().add(range);
-							}
-						}
-					}
+					readAddressRanges(subnet, ranges);
 
 					// look for options
-					Elements optionss = subnetElt.getChildElements("options");
-					if (optionss.size() > 1) {
-						throw new ConfigException("too many options sections: "+optionss.size());
+					Elements optionsRoot = subnetElt.getChildElements("options");
+					if (optionsRoot.size() > 1) {
+						throw new ConfigException("too many options sections: "+optionsRoot.size());
 					}
-					if (optionss.size() == 1) {
-						Elements options = optionss.get(0).getChildElements();
-						for (int j=0; j<options.size(); j++) {
-							try {
-								Element option = options.get(j);
-								String optionName = option.getLocalName();
-								byte code = DHCPConstants.DHO_PAD;
-								if (optionName.equals("option")) {
-									// get "code" attribute
-									String codeAttr = option.getAttributeValue("code");
-									if (codeAttr == null) {
-										throw new ConfigException("no code attrtibute for "+getElementPath(option));
-									}
-									code = Byte.parseByte(codeAttr);
-								} else if (optionName.startsWith("option-")) {		// option prefixed with "option"
-									String dhcpOptionName = "DHO_"+optionName.substring("option-".length()).toUpperCase().replace('-', '_');
-									Byte codeByte = DHCPConstants.getDhoNamesReverse(dhcpOptionName);
-									if (codeByte != null) {
-										code = codeByte;
-									} else {
-										throw new ConfigException("unknow dhcp option: "+optionName);
-									}
-								} else {			// ignoring anything else
-									logger.warning("ignoring non-option: "+getElementPath(option));
-								}
-								logger.finest("option: code="+code);
-								
-								byte[] buf = readOptionValue(option);
-								// we now create the option
-							} catch (NumberFormatException e) {
-								logger.log(Level.WARNING, "bad code attribute format", e);
-							} catch (ConfigException e) {
-								logger.log(Level.WARNING, "error parsing option", e);
-							}
-						}
+					if (optionsRoot.size() == 1) {
+						DHCPOption[] dhcpOptions = readOptionElements(optionsRoot.get(0).getChildElements());
+						// store the DHCPOPtion liste in the subnet
+						subnet.setDhcpOptions(dhcpOptions);
 					}
 				} catch (ConfigException e) {
 					logger.log(Level.WARNING, "error reading subnet configuration", e);
@@ -197,8 +146,98 @@ public final class GlobalConfigReader {
 			throw new ConfigException("IO exception", e);
 		}
 	}
+
+	private static void readAddressRanges(Subnet subnet, Elements ranges) {
+		for (int j=0; j<ranges.size(); j++) {
+			AddressRange range = null;
+			try {
+				Element rangeElt = ranges.get(j);
+				String rangeStart = rangeElt.getAttributeValue("start");
+				String rangeEnd = rangeElt.getAttributeValue("end");
+				if (rangeStart == null) {
+					throw new ConfigException("range @start missing in "+getElementPath(rangeElt));
+				}
+				if (rangeEnd == null) {
+					throw new ConfigException("range @end missing in "+getElementPath(rangeElt));
+				}
+				if (logger.isLoggable(Level.FINEST)) {
+					logger.finest("range start: "+rangeStart+", range end: "+rangeEnd+", from "+getElementPath(rangeElt));
+				}
+				
+				range = new AddressRange(InetAddress.getByName(rangeStart),
+										 InetAddress.getByName(rangeEnd));
+			} catch (ConfigException e) {
+				logger.log(Level.WARNING, "address range is invalide", e);
+			} catch (UnknownHostException e) {
+				logger.log(Level.WARNING, "error parsing address", e);
+			} finally {
+				if (range != null) {
+					subnet.getAddrRanges().add(range);
+				}
+			}
+		}
+
+	}
 	
-	public static byte[] readOptionValue(Element option) throws ConfigException, IOException {
+	/**
+	 * Read the "option" elements from the "options" section in the XML config file.
+	 * 
+	 * @param options list of "option" elements
+	 * @return the <tt>DHCPOption[]</tt> list
+	 * @throws IOException should not happen
+	 */
+	private static DHCPOption[] readOptionElements(Elements options) throws IOException {
+		List<DHCPOption> dhcpOptions = new LinkedList<DHCPOption>();
+		optionloop: for (int j=0; j<options.size(); j++) {
+			try {
+				Element option = options.get(j);
+				String optionName = option.getLocalName();
+				byte code;
+				DHCPOption dhcpOption;
+				
+				if (optionName.equals("option")) {
+					// get "code" attribute
+					String codeAttr = option.getAttributeValue("code");
+					if (codeAttr == null) {
+						throw new ConfigException("no code attrtibute for "+getElementPath(option));
+					}
+					code = Byte.parseByte(codeAttr);
+				} else if (optionName.startsWith("option-")) {		// option prefixed with "option"
+					String dhcpOptionName = "DHO_"+optionName.substring("option-".length()).toUpperCase().replace('-', '_');
+					Byte codeByte = DHCPConstants.getDhoNamesReverse(dhcpOptionName);
+					if (codeByte != null) {
+						code = codeByte;
+					} else {
+						throw new ConfigException("unknow dhcp option: "+optionName);
+					}
+				} else {			// ignoring anything else
+					logger.warning("ignoring non-option: "+getElementPath(option));
+					continue optionloop;
+				}
+				logger.finest("option: code="+code);
+				
+				dhcpOption = readOptionValue(code, option);
+				if (dhcpOption != null) {
+					dhcpOptions.add(dhcpOption);
+				}
+			} catch (NumberFormatException e) {
+				logger.log(Level.WARNING, "bad code attribute format", e);
+			} catch (ConfigException e) {
+				logger.log(Level.WARNING, "error parsing option", e);
+			}
+		}
+		return dhcpOptions.toArray(DHCPOPTION_0);
+	}
+	
+	/**
+	 * Read the value sub-portion of an option element
+	 * 
+	 * @param option
+	 * @return
+	 * @throws ConfigException
+	 * @throws IOException
+	 */
+	private static DHCPOption readOptionValue(byte code, Element option) throws ConfigException, IOException {
 		if (option == null) {
 			throw new NullPointerException("option is null");
 		}
@@ -212,6 +251,21 @@ public final class GlobalConfigReader {
 			String valueName = valueElt.getLocalName();
 			if (valueName.equals("value-byte")) {
 				outputStream.writeByte(Byte.parseByte(valueElt.getValue()));
+			} else if (valueName.equals("value-short")) {
+				outputStream.writeShort(Short.parseShort(valueElt.getValue()));
+			} else if (valueName.equals("value-int")) {
+				outputStream.writeInt(Integer.parseInt(valueElt.getValue()));
+			} else if (valueName.equals("value-inet")) {
+				outputStream.write(InetAddress.getByName(valueElt.getValue()).getAddress());
+			} else if (valueName.equals("value-string")) {
+				outputStream.writeBytes(valueElt.getValue());
+			} else if (valueName.equals("value-string-item")) {
+				String stringItem = valueElt.getValue();
+				if (stringItem.length() > 255) {
+					throw new ConfigException("length of string-item is > 255: "+valueElt.getValue());
+				}
+				outputStream.writeByte(stringItem.length());
+				outputStream.writeBytes(stringItem);
 			} else if (valueName.equals("mirror")) {
 				mirrorDetected++;
 			}
@@ -220,15 +274,15 @@ public final class GlobalConfigReader {
 		if (mirrorDetected > 1) {
 			throw new ConfigException("too many mirror elements ("+mirrorDetected+") at "+getElementPath(option));
 		}
-		if (mirrorDetected == 1) {
-			if (byteOutput.size() > 1) {
-				throw new ConfigException("when 'mirror' is present, there must not be any other content at "+getElementPath(option));
-			}
-			return null;
-		}
-		return byteOutput.toByteArray();
+		return new DHCPOption(code, byteOutput.toByteArray(), mirrorDetected > 0);
 	}
 	
+	/**
+	 * Print the element's path in the document, for debugging and logging purpose
+	 * 
+	 * @param element base element
+	 * @return the pseudo xpath of the element
+	 */
 	public static String getElementPath(Element element) {
 		String path = "";
 		Element child = element;
@@ -320,4 +374,6 @@ public final class GlobalConfigReader {
     		logger.log(Level.SEVERE, "config exception", e);
     	}
     }
+    
+    private static final DHCPOption[] DHCPOPTION_0 = new DHCPOption[0];
 }
