884556d637a57a5318b871d7adb2c8c42fb0d452
shadinger <shadinger@users.noreply.github.com>
2006-09-26 11:37:13 +0000

    Canceled last modification.

--- src/org/dhcp4java/DHCPPacket.java
+++ src/org/dhcp4java/DHCPPacket.java
@@ -2231,14 +2231,7 @@ public class DHCPPacket implements Cloneable, Serializable {
         appendHex(sbuf, (byte) ((i & 0x000000ff)       ));
     }
 
-    /**
-     * Transform a <tt>String</tt> to a <tt>byte[]</tt> sequence, using transparent
-     * encoding.
-     * 
-     * @param str the string to convert
-     * @return the byte[] sequence
-     */
-    public static byte[] stringToBytes(String str) {
+    static byte[] stringToBytes(String str) {
         if (str == null) { return null; }
 
         char[] chars = str.toCharArray();
