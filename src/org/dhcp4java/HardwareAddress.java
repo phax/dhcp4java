8d7098873eaf5e5a2794f36c16cd12689f9fb894
shadinger <shadinger@users.noreply.github.com>
2006-11-08 21:08:35 +0000

    New class HardwareAddress.

---
+++ src/org/dhcp4java/HardwareAddress.java
@@ -0,0 +1,131 @@
+/*
+ *	This file is part of dhcp4java, a DHCP API for the Java language.
+ *	(c) 2006 Stephan Hadinger
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation; either
+ *	version 2.1 of the License, or (at your option) any later version.
+ *
+ *	This library is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *	Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Lesser General Public
+ *	License along with this library; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.dhcp4java;
+
+import static org.dhcp4java.server.config.xml.Util.getElementPath;
+
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+
+import nu.xom.Element;
+import nu.xom.Elements;
+
+import org.dhcp4java.server.Subnet;
+import org.dhcp4java.server.config.ConfigException;
+
+/**
+ * Class is immutable.
+ * 
+ * @author Stephan Hadinger
+ * @version 0.60
+ */
+public class HardwareAddress implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private final byte[] hardwareAddress;
+	
+	/*
+	 * Invariants:
+	 * 	1- hardwareAddress is not null
+	 */
+	
+	public HardwareAddress(byte[] macAddr) {
+		this.hardwareAddress = macAddr;
+	}
+	
+	/**
+	 * 
+	 * <p>Object is cloned to avoid any side-effect.
+	 */
+	public byte[] getHardwareAddress() {
+		return hardwareAddress.clone();
+	}
+
+	/**
+	 * Prints the hardware address in hex format, split by ":".
+	 */
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer(17);
+		for (int i=0; i<hardwareAddress.length; i++) {
+            sb.append(Character.forDigit((hardwareAddress[i]) & 0xf, 16))
+              .append(Character.forDigit( hardwareAddress[i]  & 0xf, 16));
+            if (i<hardwareAddress.length-1) {
+            	sb.append(":");
+            }
+		}
+		return sb.toString();
+	}
+	
+	/**
+	 * Parse the MAC address in hex format, split by ':'.
+	 * 
+	 * <p>E.g. <tt>0:c0:c3:49:2b:57</tt>.
+	 * 
+	 * @param macAddr
+	 * @return
+	 */
+	public static HardwareAddress getHardwareAddressByString(String macStr) {
+		if (macStr == null) {
+			throw new NullPointerException("macStr is null");
+		}
+		String[] macAdrItems = macStr.split(":");
+		if (macAdrItems.length != 6) {
+			throw new IllegalArgumentException("macStr["+macStr+"] has not 6 items");
+		}
+		byte[] macBytes = new byte[6];
+		for (int i=0; i<6; i++) {
+			int val = Integer.parseInt(macAdrItems[i], 16);
+			if ((val < -128) || (val > 255)) {
+				throw new IllegalArgumentException("Value is out of range:"+macAdrItems[i]);
+			}
+			macBytes[i] = (byte) val;
+		}
+		return new HardwareAddress(macBytes);		
+	}
+
+	/**
+	 * Parse the MAC address in hex format, split by ':'.
+	 * 
+	 * <p>E.g. <tt>0:c0:c3:49:2b:57</tt>.
+	 * @param macStr
+	 * @return
+	 * @throws ConfigException
+	 */
+	private static final byte[] parseHardwareAddress(String macStr) throws ConfigException {
+		if (macStr == null) {
+			throw new NullPointerException("macStr is null");
+		}
+		String[] macAdrItems = macStr.split(":");
+		if (macAdrItems.length != 6) {
+			throw new ConfigException("macStr["+macStr+"] has not 6 items");
+		}
+		byte[] macBytes = new byte[6];
+		for (int i=0; i<6; i++) {
+			int val = Integer.parseInt(macAdrItems[i], 16);
+			if ((val < -128) || (val > 255)) {
+				throw new ConfigException("Value is out of range:"+macAdrItems[i]);
+			}
+			macBytes[i] = (byte) val;
+		}
+		return macBytes;
+	}
+}
