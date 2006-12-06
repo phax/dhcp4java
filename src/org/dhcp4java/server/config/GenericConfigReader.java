ad058976062029899f7e44e00809672a89b178c5
shadinger <shadinger@users.noreply.github.com>
2006-12-06 09:07:03 +0000

    GenericConfigReader created.

---
+++ src/org/dhcp4java/server/config/GenericConfigReader.java
@@ -0,0 +1,51 @@
+/*
+ *	This file is part of dhcp4java, a DHCP API for the Java language.
+ *	(c) 2006 Stephan Hadinger
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation; either
+ *	version 2.1 of the License, or (at your option) any later version.
+ *
+ *	This library is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *	Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Lesser General Public
+ *	License along with this library; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.dhcp4java.server.config;
+
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import org.dhcp4java.DHCPCoreServer;
+import org.dhcp4java.server.config.xml.ClusterMainConfigReader;
+
+/**
+ * 
+ * @author Stephan Hadinger
+ * @version 0.70
+ */
+public abstract class GenericConfigReader {
+	
+	private static final Logger logger = Logger.getLogger(GenericConfigReader.class.getName().toLowerCase());
+
+	
+	public GenericConfigReader(DHCPCoreServer server, Properties props) {
+		
+	}
+	
+	public abstract GlobalConfig loadGlobalConfig();
+	
+	public abstract FrontendConfig loadFrontEndConfig();
+	
+	public abstract TopologyConfig loadTopologyConfig();
+	
+	public TopologyConfig reloadTopologyConfig() {
+		return loadTopologyConfig();
+	}
+
+}
