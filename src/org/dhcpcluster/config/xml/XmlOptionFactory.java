d6102fe361eae0a1da49f5e7dc15bbea236f19d7
shadinger <shadinger@users.noreply.github.com>
2007-01-02 10:40:17 +0000

    Small improvement.

--- src/org/dhcpcluster/config/xml/XmlOptionFactory.java
+++ src/org/dhcpcluster/config/xml/XmlOptionFactory.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.xml.bind.JAXBElement;
@@ -221,6 +220,6 @@ final class DHCPOptionBuilder {
 	}
 			
 	public DHCPOption getResultDHCPOption() {
-		return new DHCPOption(code, byteOutput.toByteArray(), mirror);
+		return new DHCPRichOption(code, byteOutput.toByteArray(), mirror);
 	}
 }
