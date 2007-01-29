ce670e16ea49bedad873b08f96d54b698bcf8466
shadinger <shadinger@users.noreply.github.com>
2007-01-29 13:59:08 +0000

    Minor changes.

--- src/org/dhcpcluster/ClusterLogFormatter.java
+++ src/org/dhcpcluster/ClusterLogFormatter.java
@@ -23,7 +23,6 @@ import java.io.StringWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Formatter;
-import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 /**
